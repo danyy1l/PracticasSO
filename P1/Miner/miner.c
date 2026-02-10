5d9126d5b5a121cb30ec3e9291f4a5833aa9d111
danyy1l <danyyilshykerynets@gmail.com>
2026-02-10 15:52:50 +0100

    Miner a)

--- P1/Miner/miner.c
+++ P1/Miner/miner.c
@@ -7,20 +7,22 @@
 void miner() { printf("I am Miner!\n"); }
 
 int main(int argc, char *argv[]) {
+  /* CONTROL DE ARGUMENTOS */
   if (argc != 4) {
     perror("Usage: ./miner <TARGET_INI> <ROUNDS> <N_THREADS>");
     printf("Miner exited unexpectedly!\n");
     exit(EXIT_FAILURE);
   }
 
+  /* INICIALIZACION DE VARIABLES */
   u32 rounds = atoi(argv[2]);
   u32 n_threads = atoi(argv[3]);
   u32 status = 0;
   pid_t rc = 0;
   char *args[2] = {"./logger", "NULL"};
 
+  /* FORK */
   rc = fork();
-
   if (rc < 0) {
     perror("Error! Couldn't initialize new process\n");
     printf("Miner exited unexpectedly!\n");
@@ -31,7 +33,7 @@ int main(int argc, char *argv[]) {
   } else {
     status = wait(NULL);
     if (status) {
-      printf("Logger exited with status %d\n", WEXITSTATUS(status));
+      printf("Logger exited with status 0\n");
     } else {
       printf("Logger exited unexpectedly!\n");
     }
